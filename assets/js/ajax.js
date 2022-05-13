74161b0fe1596811227c21e5277966deda757477
Mariotti <prog42@ixcsoft.com.br>
2022-05-13 16:51:08 -0300

    exemplo abrir modal e editar html

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,13 +3,16 @@ $('.triggerModal').click(function(){
 	let dataRequest = {
 		action: action
 	}
-	let request = $.ajax({
-		url: 'action.php',
-		type: 'POST',
-		data: dataRequest,
-		dataType: 'JSON'
-	}).done(function(response){
-		$('#modal1 .modal-content').html(response.html);
-		$('#modal1').modal('open');
-	});
+	$('#modal1 .modal-header').html('<h3>VAMOOOOOOO</h3>');
+	$('#modal1 .modal-content').html('<h5>pra frenteeeeeeee</h5>');
+	$('#modal1').modal('open');
+	// let request = $.ajax({
+	// 	url: 'action.php',
+	// 	type: 'POST',
+	// 	data: dataRequest,
+	// 	dataType: 'JSON'
+	// }).done(function(response){
+	// 	$('#modal1 .modal-content').html(response.html);
+	// 	$('#modal1').modal('open');
+	// });
 });
