121717085560be72c91056787f47226e1d00322d
Mariotti <prog42@ixcsoft.com.br>
2022-07-05 11:19:15 -0300

    ajuste nos autoload

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 	spl_autoload_register(function($class){
-		require_once 'src\\'.$class.'.php';
+        $class = str_replace('\\', '/', $class);
+		require_once 'src/'.$class.'.php';
 	});
 
 	use Services\SessionService;
